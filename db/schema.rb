1da566dc0386f4ce777764068a310e8388b7d67f
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-08-18 13:52:27 -0600

    Add Posts Controller, Model and Update README

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818185433) do
+ActiveRecord::Schema.define(version: 20150818194818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 20150818185433) do
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "published_at"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",     null: false
@@ -50,4 +61,5 @@ ActiveRecord::Schema.define(version: 20150818185433) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "posts", "users"
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :post do
+    name "MyString"
+content "MyText"
+published_at "2015-08-18 13:48:18"
+user nil
+  end
+
+end
