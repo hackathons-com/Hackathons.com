39303cb3d577b5d1f3eb376662e0afabe80871ba
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-08-18 13:19:58 -0600

    Add test factory and validations to User spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "admin@example.com"
+    password "startadmin"
+    password_confirmation "startadmin"
+    role "admin"
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it { have_secure_password }
+ it { validate_presence_of :email }
+ it { validate_presence_of :password }
+ it { validate_presence_of :password_confirmation }
+ it { validate_uniqueness_of :email }
 end
