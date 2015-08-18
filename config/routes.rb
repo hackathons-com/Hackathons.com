da56c9f1efa3afbe920d85955039bbc7e4939826
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-08-18 12:24:13 -0600

    Add Schema, routes and routing spec dir

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'application#welcome'
+  get '/about', :to => 'application#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/routing/application_routing_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, :type => :controller do
+		it 'gets the root route' do
+			expect(:get => '/').to route_to('application#welcome')
+		end
+		it 'get the /about route' do
+			expect(:get => '/about').to route_to('application#about')
+		end
+end
