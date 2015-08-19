14c636f07d1f7f86eb99c3e9937bdf8bf2bc7ec8
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-08-19 08:43:12 -0600

    Refactored the User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- it { have_secure_password }
- it { validate_presence_of :email }
- it { validate_presence_of :password }
- it { validate_presence_of :password_confirmation }
- it { validate_uniqueness_of :email }
- it { have_many :posts }
+  it 'has secure password' do
+    have_secure_password
+  end
+  it 'validates presences of email' do
+    validate_presence_of :email
+  end
+  it 'validates presences of password' do
+    validate_presence_of :password
+  end
+  it 'validates presences of password confirmation' do
+    validate_presence_of :password_confirmation
+  end
+  it 'validates the uniqueness of email' do
+    validate_uniqueness_of :email
+  end
+  it 'has many blog posts' do
+    have_many :posts
+  end
 end
